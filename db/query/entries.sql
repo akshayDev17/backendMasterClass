b9856da5f0495fc67bd9fe78c92df016f4240c09
Akshay Prabhakant <akshayprabhakant@Akshays-MacBook-Pro.local>
2022-09-21 11:11:49 +0530

    added sql queries for model definition of entry and transfer

---
+++ db/query/entries.sql
@@ -0,0 +1,13 @@
+-- name: CreateEntry :one
+INSERT INTO entries (
+  account_id, 
+  amount
+) VALUES (
+  $1, $2
+)
+RETURNING *; 
+/* '*' so all fields returned */
+
+-- name: GetEntry :one
+SELECT * FROM entries
+WHERE id = $1 LIMIT 1;
