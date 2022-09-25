83b144058939ae0366ede67d6f5bbf2d9ecb416d
Akshay Prabhakant <akshayprabhakant@Akshays-MacBook-Pro.local>
2022-09-25 14:17:04 +0530

    fix: sharedlock problem and deadlock with FOR NO KEY UPDATE locking clause, made update statement for accounts with sqlc arguments

--- db/query/accounts.sql
+++ db/query/accounts.sql
@@ -11,7 +11,8 @@ RETURNING *;
 
 -- name: GetAccount :one
 SELECT * FROM accounts
-WHERE id = $1 LIMIT 1;
+WHERE id = $1 LIMIT 1 
+FOR NO KEY UPDATE;
 
 -- name: ListAccount :many
 SELECT * FROM accounts
@@ -23,6 +24,12 @@ OFFSET $2;
 UPDATE accounts
 set balance = $2
 WHERE id = $1
+RETURNING *;
+
+-- name: AddAccountBalance :one
+UPDATE accounts
+set balance = balance + sqlc.arg(amount)
+WHERE id = sqlc.arg(id)
 RETURNING *;
 
 -- name: DeleteAccount :exec
